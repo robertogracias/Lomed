805e5ef18f9d46dd4f6cf608b7a67168bb44642e
Roberto Gracias <roberto.gracias@strategi-k.com>
2019-03-08 05:50:09 -0600

    Lomed

---
+++ optica_sv/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Optica SV",
+    "category": 'Sales',
+    "summary": """
+       Localizacion de Opticas .""",
+    "description": """
+	   Registra la orden de produccion para un laboratorio optico
+	
+    """,
+    "sequence": 1,
+    "author": "Strategi-k",
+    "website": "http://strategi-k.com",
+    "version": '12.0.0.4',
+    "depends": ['sale'],
+    "data": [
+        'security/ir.model.access.csv'
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+
+}
