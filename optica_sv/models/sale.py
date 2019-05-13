03190b0845eb31ec09c91c7efdef3d374e96ca2b
Roberto Gracias <roberto.gracias@strategi-k.com>
2019-05-13 04:03:23 -0600

    update 6

--- optica_sv/models/sale.py
+++ optica_sv/models/sale.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from odoo.exceptions import UserError, ValidationError
 from odoo.tools import float_is_zero, float_compare, DEFAULT_SERVER_DATETIME_FORMAT
 from odoo import SUPERUSER_ID
-from numero_letras import numero_a_letras, numero_a_moneda
+#from numero_letras import numero_a_letras, numero_a_moneda
 
 
 class Sucursal(models.Model):
